8e4479915fdc671feeee197935ffd683f3749c8d
muhamad.zolfaghari <you@example.com>
2022-08-20 22:00:53 +0430

    The second test was changed.

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,15 +6,17 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
+test('submitting the form calls onSubmit with username and password', async () => {
+  let submittedData
+
+  const handleSubmit = (data) => (submittedData = data)
+
+  render(<Login onSubmit={handleSubmit} />)
+
+  const username = screen.getByLabelText(/username/i)
+  const password = screen.getByLabelText(/password/i)
+
+  await userEvent.type
   // 🐨 use `await userEvent.type...` to change the username and password fields to
   //    whatever you want
   //
